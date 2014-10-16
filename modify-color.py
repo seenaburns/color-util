b27ecbceb5bd16c15f2494cd51218c5c5f7347d6
Seena Burns <hello@seenaburns.com>
2014-10-16 14:52:16 -0700

    Hex conversion + tests, add test-error

--- modify-color.py
+++ modify-color.py
@@ -6,11 +6,45 @@
 import sys
 import colorsys
 
+def error(reason):
+    print(reason)
+    sys.exit(1)
+
 class Color(object):
-    def __init__(self, color_str, color_format="hex"):
-        pass
+    """Color object, stores colors internally as RGB float"""
+    def __init__(self, color_str, color_format="HEX"):
+        self._rgb = (0, 0, 0)
+        if color_format == "HEX":
+            self._rgb = self._hex_to_rgb(color_str)
+
+    @property
+    def rgb_float(self):
+        r, g, b = self._rgb
+
+        return (round(r,3), round(g, 3), round(b, 3))
+
+    def _hex_to_rgb(self, c):
+        # Remove hash if necessary
+        if c[0] == '#':
+            c = c[1:]
+
+        # Check length
+        if len(c) != 6:
+            error('Invalid hex input: %s' % (c))
+
+        # Return RGB from hex
+        try:
+            rgb = (int(c[0:2], 16) / 255.0,
+                   int(c[2:4], 16) / 255.0,
+                   int(c[4:6], 16) / 255.0)
+            return rgb
+        except Exception:
+            error('Invalid hex input: %s' % (c))
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1 or sys.argv[1] in ['help', '--help']:
+    if len(sys.argv) <= 1 or sys.argv[1] in ['help', '--help']:
         print(__doc__)
         sys.exit()
+
+    c = Color(sys.argv[1])
+    print(c.rgb_float)
--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 testnum=0
 
+# test to see if output of cmd (arg 1) matches expected output (arg 2)
+# arg 3: optional test description
 test () {
     testnum=$[ $testnum + 1]
 
     cmd=$1
     expected=$2
-    output=$(eval $cmd)
+    output=$(eval $cmd | tr -d '\n')
     description=${3-""}
     if [[ $expected != $output ]]
     then
@@ -18,6 +20,27 @@ test () {
     fi
 }
 
+# Test to see if exit code of cmd (arg 1) is 1
+# arg 2: optional documentation
+test-error() {
+    testnum=$[ $testnum + 1]
+
+    cmd=$1
+    description=${2-""}
+
+    eval $cmd >/dev/null
+
+    if [[ $? != "1" ]]
+    then
+        echo "[ FAIL ] $testnum $description"
+        echo "  output  : 0"
+        echo "  expected: 1"
+    else
+        echo "[ PASS ] $testnum $description"
+    fi
+}
+
+# Execute all tests over specified python version (arg 2)
 run-tests () {
     defaultpython="python3"
     pythonversion="${1-$defaultpython}"
@@ -29,6 +52,11 @@ run-tests () {
 
     # Docs
     test "$py modify-color.py | head -1" "modify-color" "docs"
+
+    # Hex conversion
+    test "$py modify-color.py '#0077ff'" "(0.0, 0.467, 1.0)" "valid hex conversion"
+    test-error "$py modify-color.py '#0077ffaa'" "invalid hex length"
+    test-error "$py modify-color.py '#00rr00'" "invalid hex chars"
 }
 
 run-tests python2

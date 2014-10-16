39bd10b5346a69ca5abf82c53e160b07c56effd1
Seena Burns <hello@seenaburns.com>
2014-10-16 14:11:24 -0700

    Test doc printing, update test suite to test python 2 and python 3

--- test.sh
+++ test.sh
@@ -7,13 +7,29 @@ test () {
     cmd=$1
     expected=$2
     output=$(eval $cmd)
+    description=${3-""}
     if [[ $expected != $output ]]
     then
-        echo "[ FAIL ] $testnum"
+        echo "[ FAIL ] $testnum $description"
         echo "  output  : $output"
         echo "  expected: $expected"
     else
-        echo "[ PASS ] $testnum"
+        echo "[ PASS ] $testnum $description"
     fi
 }
 
+run-tests () {
+    defaultpython="python3"
+    pythonversion="${1-$defaultpython}"
+    py="/bin/$pythonversion"
+
+    testnum=0
+
+    echo "-------------- $pythonversion --------------"
+
+    # Docs
+    test "$py modify-color.py | head -1" "modify-color" "docs"
+}
+
+run-tests python2
+run-tests python3
